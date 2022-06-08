7e9288ed26673cd97fbf424597796cfe70c521bf
bnagel <nagelb500@gmail.com>
2022-06-08 12:23:51 -0400

    altered route structure

--- Develop/routes/notes.js
+++
@@ -1,6 +0,0 @@
-const notes = require('express').Router();
-const fs = require('fs');
-const uniqid = require('uniqid');
-const db = require('../db/db.json');
-
-module.exports = notes;
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const uniqid = require('uniqid');
 const fs = require('fs');
 const path = require('path');
-const api = require('./develop/db/db.json')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
