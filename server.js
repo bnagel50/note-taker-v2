a2d32411236f5076e9f44b359070605d4391f85b
bnagel <nagelb500@gmail.com>
2022-06-08 12:10:14 -0400

    pathing test

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const uniqid = require('uniqid');
 const fs = require('fs');
 const path = require('path');
+const api = require('./develop/public/assets/js/index.js')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/api', api);
 
 app.use(express.static('public'));
 
